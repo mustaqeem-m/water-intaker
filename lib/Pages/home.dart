3e1614d8a0aca23741557f6d1cd1917a090fe6f7
mustaqeem-m <mmmustaqeem1910@gmail.com>
2025-06-27 07:51:39 +0530

    Added alert dialogue and textFeild

--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -7,7 +7,40 @@ class HomePage extends StatefulWidget {
   State<HomePage> createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomePageState extends State<HomePage> { 
+
+  final amountController = TextEditingController();
+  void addWater() {
+    showDialog(context: context,
+     builder: (context) => AlertDialog(
+      title: Text('Add Water'),
+      content: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Text('Add water to your daily intake'),
+          SizedBox(height: 10,),
+          TextField(
+            controller: amountController,
+            keyboardType: TextInputType.numberWithOptions(signed: false, decimal: true),
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: "Amount" ),
+          )
+        ]
+      ),
+      actions: [
+        TextButton(onPressed: () {
+          Navigator.pop(context);
+        }, child: Text('Cancel')),
+        TextButton(onPressed: () {
+          
+        }, child: Text('Save'))
+      ],
+     )
+    );
+
+  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,7 +53,7 @@ class _HomePageState extends State<HomePage> {
         title: Text("Water"),
       ),
       backgroundColor: Theme.of(context).colorScheme.background,
-      floatingActionButton: FloatingActionButton(onPressed: () {},child: Icon(Icons.add),),
+      floatingActionButton: FloatingActionButton(onPressed: () {addWater();},child: Icon(Icons.add),),
     );
   }
 }
